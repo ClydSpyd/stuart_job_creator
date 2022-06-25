a5b0f82e0155846fecfc861e7cebddbe4ffe9562
Dave <accedo@Daves-Machine.local>
2022-06-25 10:58:41 +0200

    unnecessary title key removed from index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const title = 'React 18 with Webpack and Babel';
 const container = document.getElementById('app');
 const root = createRoot(container);
 
-root.render(<App title={title} />);
+root.render(<App />);
